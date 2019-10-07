d3de0ecc2a4fd3d24c361a53e56392e727656bb6
Robert Biggs <rbiggs@ymail.com>
2019-10-07 09:21:23 -0700

    Use latest version of @composi/core
    1. Updated resources to use latest version of @composi/core.

--- resources/src/js/components/title.js
+++ resources/src/js/components/title.js
@@ -2,8 +2,10 @@ import { h } from '@composi/core'
 
 export function Title({greeting}) {
   return (
-    <nav>
-      <h1 class="header--main">Hello, {greeting}!</h1>
-    </nav>
+    <header>
+      <nav>
+        <h1 class="header--main">Hello, {greeting}!</h1>
+      </nav>
+    </header>
   )
 }
