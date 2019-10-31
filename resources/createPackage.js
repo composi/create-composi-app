ddf455c34d1aa3c9965d1b424372ba14892c69c7
Robert Biggs <rbiggs@ymail.com>
2019-10-31 10:26:00 -0700

    Update to latest version of @composi/core.

--- resources/createPackage.js
+++ resources/createPackage.js
@@ -26,7 +26,7 @@ module.exports = opts => {
     "@babel/preset-env": "^7.6.0",
     "@composi/are-equal": "^1.0.4",
     "@composi/clone": "^1.0.1",
-    "@composi/core": "^2.5.9",
+    "@composi/core": "^2.5.10",
     "@composi/gestures": "^1.0.6",
     "@composi/get-type": "^1.0.8",
     "@composi/idb": "^1.0.8",
