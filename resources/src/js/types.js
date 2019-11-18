73774585627f934454cd1c6e936c109f750db454
Robert Biggs <rbiggs@ymail.com>
2019-11-18 09:41:48 -0800

    Improved typing for Children type.

--- resources/src/js/types.js
+++ resources/src/js/types.js
@@ -24,7 +24,7 @@ export function noop() {}
  * @typedef {import('@composi/core').Send} Send
  * @typedef {import('@composi/core').Program} Program
  * @typedef {import('@composi/core').VNode} VNode
- * @typedef {unknown} Children
+ * @typedef {string | number | undefined | null | VNode | VNode[]} Children
  * @typedef {import('@composi/core').Props} Props
  * @typedef {() => State} GetState
  */
