3393e0c04d32ef285febf9071abe5566a1e898df
Robert Biggs <rbiggs@ymail.com>
2019-11-18 07:20:00 -0800

    Use latest version of @composi/core.
    1. Latest version of @composi/core has update for index.d.ts that provides proper typing for JSX to allow child content inserted in JSX slots.
    2. Updated types.js with type 'Children' to allow typing component children.

--- resources/src/js/types.js
+++ resources/src/js/types.js
@@ -24,6 +24,7 @@ export function noop() {}
  * @typedef {import('@composi/core').Send} Send
  * @typedef {import('@composi/core').Program} Program
  * @typedef {import('@composi/core').VNode} VNode
+ * @typedef {unknown} Children
  * @typedef {import('@composi/core').Props} Props
  * @typedef {() => State} GetState
  */
