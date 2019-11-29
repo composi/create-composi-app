f94026681e7a1137572faac75b85b059dbda3bbf
Robert Biggs <rbiggs@ymail.com>
2019-11-29 07:09:47 -0800

    Use latest version of @composi/core.

--- resources/createPackage.js
+++ resources/createPackage.js
@@ -26,7 +26,7 @@ module.exports = opts => {
     "@babel/preset-env": "^7.6.0",
     "@composi/are-equal": "^1.0.8",
     "@composi/clone": "^1.0.1",
-    "@composi/core": "^2.6.3",
+    "@composi/core": "^2.7.0",
     "@composi/gestures": "^1.0.6",
     "@composi/get-type": "^1.0.9",
     "@composi/idb": "^1.0.8",
